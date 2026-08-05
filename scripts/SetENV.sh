89ce8bef3dfbf10635b7a271a9d5035f704af84e
夜花櫻 <yanasakuranight@gmail.com>
2026-08-05 17:40:00 +0800

    Fix Lophinya release artifact naming [skip ci]

--- scripts/SetENV.sh
+++ scripts/SetENV.sh
@@ -2,15 +2,18 @@ prop() {
   grep "^[[:space:]]*${1}" gradle.properties | cut -d'=' -f2 | sed 's/^[[:space:]]*//; s/\r//'
 }
 
-project_id="lophine"
-project_id_b="Lophine"
+# The Gradle modules and upstream branding still use "lophine" internally.
+# Public release artifacts belong to this fork and should be named Lophinya.
+project_id="lophinya"
+project_id_b="Lophinya"
+build_output_prefix="lophine"
 
 commitid=$(git log --pretty='%h' -1)
 mcversion=$(prop mcVersion)
 release=$(prop release)
 pushRepo=$(prop pushRepo)
-release_tag="$mcversion-$commitid"
-jarName="$project_id-$mcversion-paperclip.jar"
+release_tag="$mcversion-nya-$commitid"
+jarName="$project_id-paperclip-$mcversion-$commitid.jar"
 jarName_dir="lophine-server/build/libs/$jarName"
 
 flag_push_repo=false
@@ -34,22 +37,25 @@ elif [ "$pushRepo" = "false" ]; then
   flag_push_repo=false
 fi
 
-actual_jar=$(ls lophine-server/build/libs/$project_id-paperclip-*.jar 2>/dev/null | head -n 1)
-if [ -n "$actual_jar" ]; then
-  mv "$actual_jar" "$jarName_dir"
-else
-  echo "Warning: No paperclip jar found to rename"
+actual_jar=$(find lophine-server/build/libs -maxdepth 1 -type f -name "$build_output_prefix-paperclip-*.jar" -print | sort | head -n 1)
+if [ -z "$actual_jar" ]; then
+  echo "::error::No Lophine paperclip build output was found to publish as Lophinya" >&2
+  exit 1
 fi
 
-echo "project_id=$project_id" >> $GITHUB_ENV
-echo "project_id_b=$project_id_b" >> $GITHUB_ENV
-echo "commit_id=$commitid" >> $GITHUB_ENV
-echo "commit_msg=$(git log --pretty='> [%h] %s' -1)" >> $GITHUB_ENV
-echo "mcversion=$mcversion" >> $GITHUB_ENV
-echo "pre=$pre" >> $GITHUB_ENV
-echo "tag=$release_tag" >> $GITHUB_ENV
-echo "jar=$jarName" >> $GITHUB_ENV
-echo "jar_dir=$jarName_dir" >> $GITHUB_ENV
-echo "flag_push_repo=$flag_push_repo" >> $GITHUB_ENV
-echo "flag_release=$flag_release" >> $GITHUB_ENV
-echo "make_latest=$make_latest" >> $GITHUB_ENV
+if [ "$actual_jar" != "$jarName_dir" ]; then
+  mv "$actual_jar" "$jarName_dir"
+fi
+
+echo "project_id=$project_id" >> "$GITHUB_ENV"
+echo "project_id_b=$project_id_b" >> "$GITHUB_ENV"
+echo "commit_id=$commitid" >> "$GITHUB_ENV"
+echo "commit_msg=$(git log --pretty='> [%h] %s' -1)" >> "$GITHUB_ENV"
+echo "mcversion=$mcversion" >> "$GITHUB_ENV"
+echo "pre=$pre" >> "$GITHUB_ENV"
+echo "tag=$release_tag" >> "$GITHUB_ENV"
+echo "jar=$jarName" >> "$GITHUB_ENV"
+echo "jar_dir=$jarName_dir" >> "$GITHUB_ENV"
+echo "flag_push_repo=$flag_push_repo" >> "$GITHUB_ENV"
+echo "flag_release=$flag_release" >> "$GITHUB_ENV"
+echo "make_latest=$make_latest" >> "$GITHUB_ENV"
